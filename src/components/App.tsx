9270fe22d39ed0bb1f4bb58121870d86472c86c4
Uladzislau Piatkevich <Uladzislau_Piatkevich@epam.com>
2023-02-16 16:46:43 +0100

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function App()
+{
+    const applicationTitle = "IMovie";
+    return <h1>{applicationTitle} react app with type script.</h1>;
+}
+
+export default App;
