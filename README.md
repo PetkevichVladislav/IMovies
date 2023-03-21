9f19e6f77693421d69ccf0f07edc661e2db0e08e
Uladzislau Piatkevich <Uladzislau_Piatkevich@epam.com>
2023-03-21 23:02:12 +0100

    Task-3 task complite

--- README.md
+++ README.md
@@ -3,9 +3,13 @@ To run the project make the next step:
 2. Run project with command "npm start".
 
 To test the project make the next step:
+
 To run unit test execute command in command line "npm run test".
+
 To run e2e test execute command in command line "npm run e2e-test".
+
 To open cypress to browse test:
+
     1. Open two command line
     2. Execute command in command line "npm start" to run project.
     3. Execute command in first command line "npm run cypress" to run cypress.
--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,17 +1,71 @@
 import "./MovieCard.scss"
 import Image from "./Bitmap.png";
 
-const MovieCard = () => {
+import { FC, useState } from "react";
+import { PopupMenu } from "../PopupMenu/PopupMenu";
+
+export interface IMovieCard {
+    imageUrl: string;
+    movieName: string;
+    releaseYear: string;
+    genres: string[];
+    onClick: () => void;
+}
+
+export const MovieCard: FC<IMovieCard> = (movie: IMovieCard) => {
+    const [isMouseHoveredOnCard, setIsMouseHoveredOnCard] = useState(false);
+    const [isPopupMenuOpened, setIsPopupMenuOpened] = useState(false);
+    const [menuItems, setMenuItems] = useState([
+        {
+            onClick: () => console.log('Edit'),
+            menuItem: 'Edit',
+        },
+        {
+            onClick: () => console.log('Delete'),
+            menuItem: 'Delete',
+        }
+    ]);
+
+    const handleClosePopupMenu = () => {
+        setIsPopupMenuOpened(false);
+        console.log("close form");
+    };
+
+    const handleMouseEnter = () => {
+        setIsMouseHoveredOnCard(true);
+    }
+
+    const handleMouseLeave = () => {
+        setIsPopupMenuOpened(false);
+        setIsMouseHoveredOnCard(false);
+    }
+
+    const handleCardClick = () => {
+        movie.onClick();
+    }
+
+    const handleMenuClick = (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+        event.stopPropagation();
+        console.log("open form");
+        setIsPopupMenuOpened(true);
+    }
+
     return (
-        <div className="movie-card">
-            <img className="movie-card__image" src={Image} alt="movie_card"/>
+        <div className="movie-card" data-testid="movie-card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleCardClick}>
+            {isMouseHoveredOnCard &&
+                <span className="movie-card__context-menu icon-more-vertical" data-testid="card-menu-item" onClick={(event) => handleMenuClick(event)}></span>
+            }
+            {isPopupMenuOpened &&
+                <div className="movie-card__popupmenu_container">
+                    <PopupMenu menuItems={menuItems} onClose={handleClosePopupMenu} />
+                </div>
+            }
+            <img className="movie-card__image" src={movie.imageUrl} alt="movie_card" />
             <div className="movie-card__description-container">
-                <p className="movie-card__description movie-card__description--title">Glitch</p>
-                <p className="movie-card__description movie-card__description--production-year">2004</p>
+                <p className="movie-card__description movie-card__description--title">{movie.movieName}</p>
+                <p className="movie-card__description movie-card__description--production-year">{movie.releaseYear}</p>
             </div>
-            <p className="movie-card__description movie-card__description--genre">Action, Music</p>
+            <p className="movie-card__description movie-card__description--genre">{movie.genres.join(", ")}</p>
         </div>
     );
 }
-
-export default MovieCard;
